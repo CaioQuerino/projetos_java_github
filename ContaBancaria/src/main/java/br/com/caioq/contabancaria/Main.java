fffde188c03678b9f3d7fff0f0082a93605b31de
Caio Querino <caioquerino04@gmail.com>
2025-09-18 23:32:29 -0300

    test: teste dos serviços funcional

--- ContaBancaria/src/main/java/br/com/caioq/contabancaria/Main.java
+++ ContaBancaria/src/main/java/br/com/caioq/contabancaria/Main.java
@@ -1,51 +1,32 @@
 package br.com.caioq.contabancaria;
         
-import java.util.Scanner;
 import br.com.caioq.contabancaria.service.ServicoConta;
+import br.com.caioq.contabancaria.service.ServicoUsuario;
 import br.com.caioq.contabancaria.model.Conta;
-
+import br.com.caioq.contabancaria.model.Usuario;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Conta conta = new Conta();
+        // Criando uma conta
+        Conta conta = new Conta("Caio", "123", 1234);
+        
+        // Criando usuário
+        Usuario usuario = new Usuario(conta);
+        
+        // Serviços
         ServicoConta servicoConta = new ServicoConta(conta);
+        ServicoUsuario servicoUsuario = new ServicoUsuario(usuario);
         
-        int opcao;
-        do {
-            System.out.println("\n=== Sistema Bancário ===");
-            System.out.println("1 - Sacar");
-            System.out.println("2 - Depositar");
-            System.out.println("3 - Consultar Saldo");
-            System.out.println("0 - Sair");
-            System.out.print("Escolha uma opção: ");
-            
-            opcao = sc.nextInt();
-            
-            switch (opcao) {
-                case 1:
-                    System.out.print("Valor para sacar: R$");
-                    double valorSaque = sc.nextDouble();
-                    servicoConta.sacar(valorSaque);
-                    break;
-                case 2:
-                    System.out.print("Valor para depositar: R$");
-                    double valorDeposito = sc.nextDouble();
-                    servicoConta.depositar(valorDeposito);
-                    break;
-                case 3:
-                    servicoConta.consultarSaldo();
-                    break;
-                case 0:
-                    System.out.println("Saindo do sistema...");
-                    break;
-                default:
-                    System.out.println("Opção inválida!");
-                    break;
-            }
-            
-        } while (opcao != 0);
+        // Tentativa de login
+        boolean logado = servicoUsuario.login("Caio", "123");
         
-        sc.close();
+        if (logado) {
+            // Operações bancárias
+            servicoConta.depositar(1000);
+            servicoConta.sacar(250);
+            servicoConta.consultarSaldo();
+        }
+        
+        System.out.println("\nTitular da conta: " + usuario.getConta().getNome());
     }
 }
