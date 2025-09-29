caeef4849285c55ec3ec606c097c29e742a91aa2
Caio Querino <caioquerino04@gmail.com>
2025-09-29 11:35:19 -0300

    feat: chamando o serviço de validar cpf ao fazer login

--- ContaBancaria/src/main/java/br/com/caioq/contabancaria/service/ServicoUsuario.java
+++ ContaBancaria/src/main/java/br/com/caioq/contabancaria/service/ServicoUsuario.java
@@ -3,14 +3,24 @@ package br.com.caioq.contabancaria.service;
 import br.com.caioq.contabancaria.model.Usuario;
 
 public class ServicoUsuario {
-    private Usuario usuario;
+    final Usuario usuario;
+    public ServicoValidadores servicoValidadores;
 
     public ServicoUsuario(Usuario usuario) {
         this.usuario = usuario;
+        this.servicoValidadores = new ServicoValidadores();
     }
 
-    public boolean login(String nome, String senha) {
-        if (usuario.getConta().getNome().equals(nome) && 
+    public boolean login(String cpf, String senha) {
+       
+       if(!servicoValidadores.validarCPF(cpf)) {
+           System.out.println("CPF invalido!");
+           return false;
+       } else {
+            System.out.println("CPF valido!");
+       }
+               
+        if (usuario.getConta().getCPF().equals(cpf) && 
             usuario.getConta().getSenha().equals(senha)) {
             System.out.println("Usuário logado com sucesso!");
             return true;
