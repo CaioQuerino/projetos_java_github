4b5d01f3259608d69dbf2c6b9b0b7a74aedea4f0
Caio Querino <caioquerino04@gmail.com>
2025-09-29 17:25:01 -0300

    test: login funcional

--- ContaBancaria/src/main/java/br/com/caioq/contabancaria/Main.java
+++ ContaBancaria/src/main/java/br/com/caioq/contabancaria/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         // Criando uma conta
         
-        Conta conta = new Conta("Paula", "24269158034", "123", 1234);
+        Conta conta = new Conta("Paula", "24269158034", "123", "1234-5", "12345-6");
         
         // Criando usuário
         Usuario usuario = new Usuario(conta);
@@ -20,12 +20,14 @@ public class Main {
         
         // Tentativa de login
         
-        boolean logado = servicoUsuario.login("24269158034", "123");
+        boolean logado = servicoUsuario.login("24269158034", "1234-5", "12345-6", "123");
         
         if (logado) {
             // Operações bancárias
             System.out.printf("Nome: %s\n", usuario.getConta().getNome());
-            System.out.printf("Agê: %s\n", usuario.getConta().getAgencia());
+            System.out.printf("Agê: %s\n", usuario.getConta().getAgencia());        
+            System.out.printf("Conta: %s\n", usuario.getConta().getConta());
+
             servicoConta.depositar(1000);
             servicoConta.sacar(250);
             servicoConta.consultarSaldo();
