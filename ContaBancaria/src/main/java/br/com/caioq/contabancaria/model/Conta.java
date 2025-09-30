909d060b9778edd8dbb37b46183c97c40ce5ab6a
Caio Querino <caioquerino04@gmail.com>
2025-09-30 12:18:12 -0300

    feat: adicionando id e titular

--- ContaBancaria/src/main/java/br/com/caioq/contabancaria/model/Conta.java
+++ ContaBancaria/src/main/java/br/com/caioq/contabancaria/model/Conta.java
@@ -1,39 +1,39 @@
 package br.com.caioq.contabancaria.model;
 
+
 public class Conta {
 
+    private Long id;
     private double saldo;
     private String senha;
     private String agencia;
-    private final String conta;
+    private String conta;
+    private Usuario titular;
+
+    public Conta() {}
 
     public Conta(String senha, String agencia, String conta) {
-
         this.senha = senha;
         this.agencia = agencia;
         this.conta = conta;
         this.saldo = 0;
     }
 
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
+    public double getSaldo() { return saldo; }
+    public void setSaldo(double saldo) { this.saldo = saldo; }
 
-    public double getSaldo() {
-        return saldo;
-    }
+    public String getSenha() { return senha; }
+    public void setSenha(String senha) { this.senha = senha; }
 
-    public void setSaldo(double saldo) {
-        this.saldo = saldo;
-    }
+    public String getAgencia() { return agencia; }
+    public void setAgencia(String agencia) { this.agencia = agencia; }
 
-    public String getSenha() {
-        return senha;
-    }
+    public String getConta() { return conta; }
+    public void setConta(String conta) { this.conta = conta; }
 
-    public String getAgencia() {
-        return agencia;
-    }
-
-    public String getConta() {
-        return conta;
-    }
+    public Usuario getTitular() { return titular; }
+    public void setTitular(Usuario titular) { this.titular = titular; }
 }
