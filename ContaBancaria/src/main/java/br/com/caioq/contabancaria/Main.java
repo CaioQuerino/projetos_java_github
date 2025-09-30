2b85fd91bbdfe7f594c7826a8fd469aadebb08d6
Caio Querino <caioquerino04@gmail.com>
2025-09-29 23:49:13 -0300

    test: login funcional

--- ContaBancaria/src/main/java/br/com/caioq/contabancaria/Main.java
+++ ContaBancaria/src/main/java/br/com/caioq/contabancaria/Main.java
@@ -1,5 +1,5 @@
 package br.com.caioq.contabancaria;
-        
+
 import br.com.caioq.contabancaria.model.Conta;
 import br.com.caioq.contabancaria.model.Usuario;
 import br.com.caioq.contabancaria.service.ServicoConta;
@@ -7,31 +7,29 @@ import br.com.caioq.contabancaria.service.ServicoUsuario;
 
 public class Main {
     public static void main(String[] args) {
-        // Criando uma conta
-        
-        Conta conta = new Conta("Paula", "24269158034", "123", "1234-5", "12345-6");
-        
         // Criando usuário
-        Usuario usuario = new Usuario(conta);
-        
+        Usuario usuario = new Usuario("Caio", "12345678901");
+
+        // Criando conta vinculada ao usuário
+        Conta conta = new Conta(usuario, "123", "1234-5", "12345-6");
+        usuario.setConta(conta);
+
         // Serviços
         ServicoConta servicoConta = new ServicoConta(conta);
         ServicoUsuario servicoUsuario = new ServicoUsuario(usuario);
-        
+
         // Tentativa de login
-        
-        boolean logado = servicoUsuario.login("24269158034", "1234-5", "12345-6", "123");
-        
+        boolean logado = servicoUsuario.login("12345678901", "1234-5", "12345-6", "123");
+
         if (logado) {
-            // Operações bancárias
-            System.out.printf("Nome: %s\n", usuario.getConta().getNome());
-            System.out.printf("Agê: %s\n", usuario.getConta().getAgencia());        
-            System.out.printf("Conta: %s\n", usuario.getConta().getConta());
+            System.out.printf("Nome: %s\n", usuario.getNome());
+            System.out.printf("Agência: %s\n", conta.getAgencia());
+            System.out.printf("Conta: %s\n", conta.getConta());
 
             servicoConta.depositar(1000);
             servicoConta.sacar(250);
             servicoConta.consultarSaldo();
-            System.out.println("\nTitular da conta: " + usuario.getConta().getNome());
+            System.out.println("\nTitular da conta: " + usuario.getNome());
         }
     }
 }
